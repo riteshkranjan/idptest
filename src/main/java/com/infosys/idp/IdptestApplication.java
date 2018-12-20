8ad900a898cf4613866279f41eab04d1f8e74c78
riteshkranjan <riteshk.ranjan@gmail.com>
2018-12-20 09:13:14 +0530

    swagger added

--- src/main/java/com/infosys/idp/IdptestApplication.java
+++ src/main/java/com/infosys/idp/IdptestApplication.java
@@ -2,12 +2,18 @@ package com.infosys.idp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IdptestApplication {
+public class IdptestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IdptestApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IdptestApplication.class);
 	}
 
 }
