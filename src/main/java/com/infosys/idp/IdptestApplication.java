5b1cf60063dff47526d13a41e544cccb2ef7740e
riteshkranjan <riteshk.ranjan@gmail.com>
2018-12-20 07:55:28 +0530

    spring boot app

---
+++ src/main/java/com/infosys/idp/IdptestApplication.java
@@ -0,0 +1,14 @@
+package com.infosys.idp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdptestApplication.class, args);
+	}
+
+}
+
