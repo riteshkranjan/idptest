6190e4828f86c29b7797f90abb792a0d9804a619
riteshkranjan <riteshk.ranjan@gmail.com>
2019-01-07 13:02:13 +0530

    single file puppet

--- etc/puppetlabs/code/environments/production/manifests/site.pp
+++ etc/puppetlabs/code/environments/production/manifests/site.pp
@@ -1,3 +1,11 @@
 node 'puppetagent1.fd3lxcr0ohmupihn355hl54zvc.rx.internal.cloudapp.net' {
-   include apache2
+  package { 'apache2':
+    ensure => installed,
+  }
+
+  service { 'apache2':
+    ensure  => true,
+    enable  => true,
+    require => Package['apache2'],
+  }
 }
